87ab98f2a611b9dc61291176e643990a1fdce2b3
jesuscreyes <jesuscreyes11@gmail.com>
2016-06-07 11:24:06 -0700

    Broke test

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -9,7 +9,7 @@ class StackSpec extends PlaySpec {
       stack.push(1)
       stack.push(2)
       stack.pop() mustBe 2
-      stack.pop() mustBe 1
+      stack.pop() mustBe 2
     }
     "throw NoSuchElementException if an empty stack is popped" in {
       val emptyStack = new Stack[Int]
