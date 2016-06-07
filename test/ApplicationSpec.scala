203bb0d7b0217929e4ca43123fb9fc3ee98e4eb4
jesuscreyes <jesuscreyes11@gmail.com>
2016-06-07 11:18:59 -0700

    Modified ApplicationSpec.scala

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -1,31 +1,21 @@
-import org.specs2.mutable._
-import org.specs2.runner._
-import org.junit.runner._
-import org.scalatest.Assertions._
+import collection.mutable.Stack
+import org.scalatestplus.play._
 
-import play.api.test._
-import play.api.test.Helpers._
+class StackSpec extends PlaySpec {
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
-@RunWith(classOf[JUnitRunner])
-class ApplicationSpec extends Specification {
-
-  "Application" should {
-
-    /*"send 404 on a bad request" in new WithApplication{
-      route(FakeRequest(GET, "/boum")) must beNone
-    }*/
-
-    "render the index page" in new WithApplication{
-      val home = route(FakeRequest(GET, "/")).get
-
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
-      assert(contentAsString(home) must contain ("Your new application is ready."))
+  "A Stack" must {
+    "pop values in last-in-first-out order" in {
+      val stack = new Stack[Int]
+      stack.push(1)
+      stack.push(2)
+      stack.pop() mustBe 2
+      stack.pop() mustBe 1
+    }
+    "throw NoSuchElementException if an empty stack is popped" in {
+      val emptyStack = new Stack[Int]
+      a [NoSuchElementException] must be thrownBy {
+        emptyStack.pop()
+      }
     }
   }
 }
