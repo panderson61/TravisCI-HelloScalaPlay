d879db4355338c55b17d0c57b5a1ee00e1ea3433
jesuscreyes <jesuscreyes11@gmail.com>
2016-07-13 14:57:10 -0700

    Testing docker-compose

--- test/ApplicationControllerSpec.scala
+++ test/ApplicationControllerSpec.scala
@@ -1,11 +1,13 @@
 import play.api.test._
+
 import scala.concurrent.Future
 import org.scalatestplus.play._
 import play.api.mvc._
 import play.api.test.Helpers._
 import controllers.Application
+import org.specs2.mock.Mockito
 
-class ApplicationControllerSpec extends PlaySpec with Results {
+class ApplicationControllerSpec extends PlaySpec with Results with Mockito {
 
   "Application Index" should {
     "should return Hello World" in {
